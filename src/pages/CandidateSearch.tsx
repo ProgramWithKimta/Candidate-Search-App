f9b279effb6d598cc1e669a79361b3c3c029d973
ProgramWithKimta <knguyen4268@gmail.com>
2025-04-27 15:45:02 -0500

    updating candidate seach file

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,46 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>();
+
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  // function for searching for a candidate by name using the github user API
+  const searchForCandidateByName = async (event: React.FormEvent, candidate_name: string) => {
+    event.preventDefault();
+    const data = await searchGithubUser(candidate_name);
+    setCurrentCandidate(data)l
+  };
+  
+  return (
+    <>
+    <section id='searchSection'>
+      <form
+        onSubmit={(event: React.FormEvent) => searchForCandidateByName(event, searchInput)}
+      >
+        <input
+          type='text'
+          name=''
+          id=''
+          placeholder='Candidate Name'
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button type='submit' id='searchBtn'>
+          Search
+        </button>
+      </form>
+    </section>
+    <FilmCard
+      currentFilm={currentFilm}
+      addToWatchList={addToWatchList}
+      addToSeenItList={addToSeenItList}
+    />
+  </>
+);
+};
+  )
 };
 
 export default CandidateSearch;
